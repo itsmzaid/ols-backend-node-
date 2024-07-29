5c62fd62e635244efb6bd1b9993fb53e0314fb76
zezo0411 <muhammad.zaidejaz04@gmail.com>
2024-07-30 01:38:28 +0500

    add refresh token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asynchandler.js";
 import jwt from "jsonwebtoken";
+
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
@@ -13,8 +14,9 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
     const user = await User.findById(decodedToken?._id).select(
-      "-passwoed -refreshToken"
+      "-password -refreshToken"
     );
     if (!user) {
       throw new ApiError(401, "Invalid access token");
