dddef06f6e92af47613d8bacc0500af3f93769d0
zezo0411 <muhammad.zaidejaz04@gmail.com>
2025-05-11 23:32:41 +0500

    file size 16kb to 5 mb

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
 
 app.use("/temp", express.static("public/temp"));
